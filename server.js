791d265af42df2ed7fb99bd052989b755f6e16bb
AloDeco <alois1997@orange.fr>
2020-05-09 17:56:04 -0400

    waiting room done

--- server.js
+++ server.js
@@ -1,53 +1,44 @@
-/*
-const http = require('http'); // Pour créer des serveurs http
-const url = require("url"); // Pour voir l'url auquel accède l'utilisateur
-
-// Fonction executée quand une requête est reçue
-const doOnConnect = function(req, res) {
-  const page = url.parse(req.url).pathname;
-  console.log(page);
-  if(page == "/"){
-  	res.writeHead(200, {"Content-Type": "text/html"});
-  	res.end('<p>Salut tout le monde ! Bienvenue sur ce site de <strong>Dixit</strong> en ligne !</p>');
-  }
-  else{
-  	res.writeHead(404);
-  	res.end('');
-  }
-}
-
-// Création du serveur
-const server = http.createServer(doOnConnect);
-*/
-
-
-
-
 const http = require('http');
 const fs = require('fs');
+const express = require('express');
+
+const app = require('express')();
+const server = require('http').Server(app);
+// Chargement de socket.io
+const io = require('socket.io')(server);
+
+var players = [];
 
 // Chargement du fichier index.html affiché au client
-const doOnConnect = function(req, res) {
+app.get('/', function(req, res) {
     fs.readFile('views/pseudo.html', 'utf-8', function(error, content) {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(content);
     });
-};
-const server = http.createServer(doOnConnect);
+});
 
-// Chargement de socket.io
-const io = require('socket.io').listen(server);
+app.get('/waiting_room', function(req, res) {
+    res.render('waiting_room.ejs', {players: players});
+});
+
+
 
 io.sockets.on('connection', function (socket, pseudo) {
+    
     // Quand un client se connecte, on lui envoie un message
-    socket.emit('message', 'Vous êtes bien connecté !');
-    // On signale aux autres clients qu'il y a un nouveau venu
-    socket.broadcast.emit('message', 'Un autre client vient de se connecter ! ');
+    //socket.emit('message', 'Vous êtes bien connecté !');
 
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session
     socket.on('pseudo', function(pseudo) {
         socket.pseudo = pseudo;
-        console.log(pseudo+" vient de se connecter.");
+        console.log(pseudo + " vient de se connecter.");
+        players.push(pseudo);
+        // On signale aux autres clients qu'il y a un nouveau venu
+        socket.broadcast.emit('new_player', pseudo);
+
+        const destination = '/waiting_room';
+        socket.emit('redirect', destination);
+
     });
 
     socket.on('disconnect', function(){
