358f533b530fc965b927745f47e4eaa71a1f6063
AloDeco <alois1997@orange.fr>
2020-05-09 16:20:56 -0400

    communication avec clients et enregistrement des pseudos

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const http = require('http'); // Pour créer des serveurs http
 const url = require("url"); // Pour voir l'url auquel accède l'utilisateur
 
@@ -17,5 +18,39 @@ const doOnConnect = function(req, res) {
 
 // Création du serveur
 const server = http.createServer(doOnConnect);
+*/
+
+
+
+
+var http = require('http');
+var fs = require('fs');
+
+// Chargement du fichier index.html affiché au client
+var server = http.createServer(function(req, res) {
+    fs.readFile('views/accueil.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket, pseudo) {
+    // Quand un client se connecte, on lui envoie un message
+    socket.emit('message', 'Vous êtes bien connecté !');
+    // On signale aux autres clients qu'il y a un nouveau venu
+    socket.broadcast.emit('message', 'Un autre client vient de se connecter ! ');
+
+    // Dès qu'on nous donne un pseudo, on le stocke en variable de session
+    socket.on('pseudo', function(pseudo) {
+        socket.pseudo = pseudo;
+        console.log(pseudo);
+    });
+
+});
+
+
 // Ecoute sur le port 50000
 server.listen(50000);
