8b4733ca4f6143a1b812bf38c34ba0466f4b3b78
Alban Recly <alban.recly@telecom-paristech.fr>
2020-05-09 13:23:07 -0400

    Commentaires + etoffage

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-const http = require('http');
+const http = require('http'); // Pour créer des serveurs http
+const url = require("url"); // Pour voir l'url auquel accède l'utilisateur
 
-const server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Salut tout le monde ! Bienvenue sur ce site de Dixit en ligne !');
-});
+// Fonction executée quand une requête est reçue
+const doOnConnect = function(req, res) {
+  const page = url.parse(req.url).pathname;
+  console.log(page);
+  if(page == "/"){
+  	res.writeHead(200, {"Content-Type": "text/html"});
+  	res.end('<p>Salut tout le monde ! Bienvenue sur ce site de <strong>Dixit</strong> en ligne !</p>');
+  }
+  else{
+  	res.writeHead(404);
+  	res.end('');
+  }
+}
+
+// Création du serveur
+const server = http.createServer(doOnConnect);
+// Ecoute sur le port 50000
 server.listen(50000);
