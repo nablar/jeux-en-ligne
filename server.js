1f3e5717257b9734050962fc801d74766947aca6
AloDeco <alois1997@orange.fr>
2020-05-10 09:20:22 -0400

    changement en 1 page

--- server.js
+++ server.js
@@ -7,42 +7,24 @@ const server = require('http').Server(app);
 // Chargement de socket.io
 const io = require('socket.io')(server);
 
-const session = require('express-session') ({
-  secret:'dixit',
-  resave:true,
-  saveUninitialized:true
-})
-
-const sharedsession = require("express-socket.io-session");
-
-app.use(session);
 
 var players = [];
 var chef; 
 
-// Chargement du fichier index.html affiché au client
+// Chargement du fichier pseudo.html affiché au client
 app.get('/', function(req, res) {
-    fs.readFile('views/pseudo.html', 'utf-8', function(error, content) {
+    fs.readFile('views/dixit.html', 'utf-8', function(error, content) {
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(content);
     });
 });
 
-app.get('/waiting_room', function(req, res) {
-    res.render('waiting_room.ejs', {players: players});
-});
-
-
-io.use(sharedsession(session, { 
-  autoSave:true
-}));
 
 io.sockets.on('connection', function (socket, pseudo) {
 
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session
     socket.on('pseudo', function(pseudo) {
-        socket.handshake.session.pseudo = pseudo;
-        socket.handshake.session.save();
+        socket.pseudo = pseudo;
         var pseudo_ok = check_pseudo(pseudo);
         if(pseudo_ok) {
           console.log(pseudo + " vient de se connecter.");
@@ -52,9 +34,8 @@ io.sockets.on('connection', function (socket, pseudo) {
           players.push(pseudo);
           // On signale aux autres clients qu'il y a un nouveau venu
           socket.broadcast.emit('new_player', pseudo);
-
-          const destination = '/waiting_room';
-          socket.emit('redirect', destination);
+          socket.emit('change_view', "B");
+          
         }
         else {
           socket.emit('message', "Ce pseudo est déjà pris, choisis-en un autre.")
@@ -62,12 +43,8 @@ io.sockets.on('connection', function (socket, pseudo) {
 
     });
 
-    socket.on('ask_pseudo', function(){
-      socket.emit('send_pseudo', socket.handshake.session.pseudo);
-    })
-
     socket.on('disconnect', function(){
-    	console.log(socket.handshake.session.pseudo + " vient de se déconnecter.");
+    	console.log(socket.pseudo + " vient de se déconnecter.");
     })
 
 });
