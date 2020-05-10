c4a06a4c1a460f8a9e42e2f1fceb6d3616aa7ce8
AloDeco <alois1997@orange.fr>
2020-05-10 16:32:10 -0400

    ajout choix des cartes à jouer

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const io = require('socket.io')(server);
 let players = [];
 let chef; 
 let index_counter;
+let chosen_cards={};
 let guesses={};
 let scores={};
 //let welcom = true;  //welcom = true if new gamers are welcomed, = false if the game already started
@@ -103,26 +104,33 @@ io.sockets.on('connection', function (socket, pseudo) {
       socket.emit('tirage', socket.main);
     });
 
-    socket.on('choix_carte', function(carte){
-    	a_defausser.push(carte);
-    	socket.carte_choisie = carte;
-    });
+
 
     socket.on('defausser', function(){
     	gestionCartes.defausserCartes(a_defausser);
     });
 
     socket.on('counter_choice', function(card, key_phrase) {
-      socket.emit('change_view', "D");
-      socket.broadcast.emit('change_view', "D");
-
+      chosen_cards[counter] = card;
+      a_defausser.push(card);
+      
       socket.emit('reveal_counter_choice', card, key_phrase);
       socket.broadcast.emit('reveal_counter_choice', card, key_phrase);
     });
 
+    socket.on('guesser_card_to_play', function(card) {
+      chosen_cards[socket.pseudo]=card;
+      a_defausser.push(card);
+      if(Object.keys(chosen_cards).length==players.length){
+        socket.emit('change_view', "D");
+        socket.broadcast.emit('change_view', "D");
+        socket.emit('start_guessing', players.length, a_defausser);
+      }
+    })
+
     socket.on('guesser_choice', function(pseudo, card) {
       guesses[pseudo]=card;
-      if(Object.keys(dictionary).length==players.length){
+      if(Object.keys(guesses).length==players.length){
         computeScores();
         socket.emit('change_view', "E");
         socket.emit('scores', scores);
