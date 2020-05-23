12b3f297099b465cb4dfe2b677229a740787dc76
AloDeco <alois1997@orange.fr>
2020-05-23 13:32:36 -0400

    Création de la base du menu de coté

--- js/dixit.js
+++ js/dixit.js
@@ -434,6 +434,19 @@ socket.on('redirect', function(destination) {
 
 
 
+/***************************** SIDE BAR MENU *****************************/
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
 
 /***************************** OTHER FUNCTIONS *****************************/
 // Use enter to submit pseudo 
